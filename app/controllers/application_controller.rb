640ddc4a8b556e9827055cbb6866e9bf66e86bd1
OLauterbach <olauterb@chicagobooth.edu>
2020-10-21 05:30:22 +0000

    Getting a 100

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,55 @@
 class ApplicationController < ActionController::Base
 
-end
+  def addition
+    render ({ :template => "math_templates/adding.html.erb"})
+  end
+
+    def waddition
+    
+      @first = params.fetch("first_num").to_f
+      @second = params.fetch("second_num").to_f
+      @sum = @first + @second
+
+      render ({ :template => "math_templates/wadding.html.erb"})
+  end
+
+def subtraction
+    render ({ :template => "math_templates/subbing.html.erb"})
+  end
+
+    def wsubtraction
+    
+      @first = params.fetch("first_num").to_f
+      @second = params.fetch("second_num").to_f
+      @sum = @second - @first
+
+      render ({ :template => "math_templates/wsubbing.html.erb"})
+  end
+
+def multiplication
+    render ({ :template => "math_templates/multiplying.html.erb"})
+  end
+
+    def wmultiplication
+    
+      @first = params.fetch("first_num").to_f
+      @second = params.fetch("second_num").to_f
+      @sum = @second * @first
+
+      render ({ :template => "math_templates/wmultiplying.html.erb"})
+  end
+
+def division
+    render ({ :template => "math_templates/dividing.html.erb"})
+  end
+
+    def wdivision
+    
+      @first = params.fetch("first_num").to_f
+      @second = params.fetch("second_num").to_f
+      @sum = @first / @second
+
+      render ({ :template => "math_templates/wdividing.html.erb"})
+  end
+
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+get("/add", { :controller => "application", :action => "addition"})
+get("/wizard_add", { :controller => "application", :action => "waddition"})
+get("/subtract", { :controller => "application", :action => "subtraction"})
+get("/wizard_subtract", { :controller => "application", :action => "wsubtraction"})
+get("/multiply", { :controller => "application", :action => "multiplication"})
+get("/wizard_multiply", { :controller => "application", :action => "wmultiplication"})
+get("/divide", { :controller => "application", :action => "division"})
+get("/wizard_divide", { :controller => "application", :action => "wdivision"})
+
+
 end
